dcdbd0ac5d52231f0082077c74539125dd8ae1f2
Naomi Robert <nrobert666@gmail.com>
2012-12-01 12:37:02 -0500

    Created basic rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetmap::Application.config.secret_token = '1d03c008545ac110f08cdad160285d98290d1b8e1edef5c11c8abdef082ed5e3573bcac4f3424898abdc99c8d6585067b3345153decca7c80d417b0e222775ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tweetmap::Application.config.session_store :cookie_store, key: '_tweetmap_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tweetmap::Application.config.session_store :active_record_store
