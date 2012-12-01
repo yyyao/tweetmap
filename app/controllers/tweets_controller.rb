3615ba9a6b704a387fd26076102e2c6f8d951814
Ying Yao <yingyao2010@gmail.com>
2012-12-01 17:30:31 -0500

    added option to change mile radius

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,6 +4,6 @@ class TweetsController < ApplicationController
 		lat = request.params[:lat]
 		lon = request.params[:lon]
 		word = request.params[:word]
-		@tweets = JSON.parse(RestClient.get("http://search.twitter.com/search.json", :params => { :q => "rails", :geocode => "#{lat},#{lon},3mi" }).body)["results"]
+		@tweets = JSON.parse(RestClient.get("http://search.twitter.com/search.json", :params => { :q => word, :geocode => "#{lat},#{lon},5mi" }).body)["results"]
 	end
 end
